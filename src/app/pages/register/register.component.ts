93e00ca3a3d12172a23553d0fe67c71955a4046f
b3n3c <gbence0013@gmail.com>
2024-05-12 17:56:22 +0200

    register fix

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from "../../shared/services/auth.service";
 import {FormControl, FormGroup} from "@angular/forms";
 import {Location} from "@angular/common";
 import {ToastrService} from "ngx-toastr";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -21,12 +22,14 @@ export class RegisterComponent {
     })
   });
 
-  constructor(private location: Location, private authService: AuthService, private toastr: ToastrService) { }
+  constructor(private location: Location, private authService: AuthService,
+              private toastr: ToastrService, private router: Router) { }
 
   onSubmit() {
     this.authService.signup(this.signUpForm.get('email')?.value as string, this.signUpForm.get('password')?.value as string).
     then(cred => {
       this.toastr.success('Successfully registered', 'Success');
+      this.router.navigateByUrl('/main');
     }).catch(error => {
       this.toastr.error('Error while trying to sign you up', 'Error');
     });
